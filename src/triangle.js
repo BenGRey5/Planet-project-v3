ec6af3849f4873d9784e104e139f05ae6345df09
BenGRey5 <bgraynolds@gmail.com>
2023-11-13 09:02:25 -0800

    add a class and a test

--- src/triangle.js
+++ src/triangle.js
@@ -1,9 +1,21 @@
-export default function Triangle(side1, side2, side3) {
-  this.side1 = side1;
-  this.side2 = side2;
-  this.side3 = side3;
+class DDday {
+  constructor(planet1, planet2, planet3, planet4, planet5){
+  this.pllanet1 =planet1;
+  this.pllanet2 =planet2;
+  this.pllanet3 =planet3;
+  this.pllanet4 =planet4;
+  this.pllanet5 =planet5;
+}
 }
 
-Triangle.prototype.checkType = function () {
-  return "I can't answer that yet!";
-};
+describe('./DDday', () => {
+  test('constructor should set planet properties', () => {
+    const instance = new DDday('Earth', 'Mars', 'Venus', 'Jupiter', 'Mercury');
+
+    expect(instance.planet1).toBe('Mercury')
+    expect(instance.planet2).toBe('Venus')
+    expect(instance.planet3).toBe('Earth')
+    expect(instance.planet4).toBe('Mars')
+    expect(instance.planet5).toBe('Jupiter')
+  });
+});
