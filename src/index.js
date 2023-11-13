5129be44dce4d7498699ecad895fe9849beb3e89
BenGRey5 <bgraynolds@gmail.com>
2023-11-13 12:01:43 -0800

    WIP

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
-import DDday from "./planet.test.js";
-
+import DDday from "./planet"; // Fix import statement
 
 function handleAgeFinderForm(event) {
   event.preventDefault();
-  document.querySelector("#response").innerText = null;
-  const Age2 = parseInt(document.querySelector("Age2").value);
-  const Age1 = parseInt(document.querySelector("Age1").value);
-  let planetList = (document.querySelector("planetList").value);
-  
+  document.querySelector("#result").innerText = null; // Fix ID here
+  const Age2 = parseInt(document.querySelector("#Age2").value); // Add #
+  const Age1 = parseInt(document.querySelector("#Age1").value); // Add #
+  let planetList = document.querySelector("#planetList").value; // Add #
+}
+
+// Attach the event listener outside the function
 window.addEventListener("load", function () {
-  document.querySelector("#AgeFinder").addEventListener("submit", handleTriangleForm);
+  document.querySelector("#AgeFinder").addEventListener("submit", handleAgeFinderForm); // Fix function name
 });
-gfgdfgfffffff
