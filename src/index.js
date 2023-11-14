94254fe9447ef99a00237d6f10cfcceffbb11e44
BenGRey5 <146392527+BenGRey5@users.noreply.github.com>
2023-11-14 06:44:45 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function handleAgeFinderForm(event) {
   event.preventDefault();
   document.querySelector("#result").innerText = null; // Fix ID here
