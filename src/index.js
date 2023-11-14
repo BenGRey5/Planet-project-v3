0bfd197c5b28e5c97231b055c0baa9dea2571535
BenGRey5 <146392527+BenGRey5@users.noreply.github.com>
2023-11-13 21:16:25 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-class DDday {
-  constructor(
-    planet1 = 'Earth',
-    planet2 = 'Mars',
-    planet3 = 'Venus',
-    planet4 = 'Jupiter',
-    planet5 = 'Mercury'
-  ) {
-    this.planet1 = planet1;
-    this.planet2 = planet2;
-    this.planet3 = planet3;
-    this.planet4 = planet4;
-    this.planet5 = planet5;
-  }
-
-  initializeWithNumbers() {
-    this.planet1 = 4.147;
-    this.planet2 = 1.62;
-    this.planet3 = 1;
-    this.planet4 = .531;
-    this.planet5 = .083;
-  }
-
-  getAgeForPlanet(planet) {
-    return this[planet.toLowerCase()] || 0;
-  }
-}
-
 function handleAgeFinderForm(event) {
   event.preventDefault();
   document.querySelector("#result").innerText = null; // Fix ID here
